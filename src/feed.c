afd40c85cee0fdbf66f3132a668c44a115926b92
Adam <xjanda26@stud.fit.vutbr.cz>
2022-11-05 14:20:05 +0100

    feed lite refactor

--- src/feed.c
+++ src/feed.c
@@ -4,18 +4,48 @@
 
 #include "feed.h"
 
-int get_and_print_feed(char *host, char *port, char *path, int is_testing) {
-    int result = connect_to_host(host, port, 0);
-    if (result) {
-        return result;
-    }
-    send_request(sock, host, port, path);
+int process_author_node (xmlDocPtr doc, xmlNodePtr authorNode) {
+    printf("\n");
+    xmlChar *key;
 
-    result = receive_data(is_testing);
-    if (result) {
-        return result;
+    while (authorNode != NULL) {
+        key = xmlNodeListGetString(doc, authorNode->children, 1);
+        printf("\t\t%s, %s\n", authorNode->name, key);
+        authorNode = authorNode->next;
     }
 
+    xmlFree(key);
+    return 0;
+}
+
+int process_entry_node(xmlDocPtr doc, xmlNodePtr node) {
+    // spracuj uzly
+    while (node != NULL) {
+        printf("%s\n", node->name);
+        if ((!xmlStrcmp(node->name, (const xmlChar *) "entry"))) {
+            xmlNodePtr entryNode = node->children;
+            while (entryNode != NULL) {
+                xmlChar *key;
+                printf("\t%s", entryNode->name);
+                if ((!xmlStrcmp(entryNode->name, (const xmlChar *) "author"))) {
+                    process_author_node(doc, entryNode->children);
+                } else {
+                    key = xmlNodeListGetString(doc, entryNode->children, 1);
+                    printf(", %s\n", key);
+                }
+                
+                xmlFree(key);
+                entryNode = entryNode->next;
+            }
+        }
+        
+        node = node->next;
+    }
+
+    return 0;
+}
+
+int process_xml() {
     // ak nepracujeme s UTF-8, musíme convertovat do UTF-8
     xmlChar *xml = xmlCharStrdup(xmlResponse);
     xmlDocPtr xmlTree;
@@ -30,39 +60,7 @@ int get_and_print_feed(char *host, char *port, char *path, int is_testing) {
             if (node == NULL) {
                 printf("Empty XML\n");
             } else {
-                // spracuj uzly
-                node = node->children;
-                while (node != NULL) {
-                    printf("%s\n", node->name);
-                    if ((!xmlStrcmp(node->name, (const xmlChar *) "entry"))) {
-                        xmlNodePtr entryNode = node->children;
-                        while (entryNode != NULL) {
-                            xmlChar *key;
-                            printf("\t%s", entryNode->name);
-                            if ((!xmlStrcmp(entryNode->name, (const xmlChar *) "author"))) {
-                                xmlNodePtr authorNode = entryNode->children;
-                                printf("\n");
-
-                                while (authorNode != NULL) {
-                                    key = xmlNodeListGetString(xmlTree, authorNode->children, 1);
-                                    printf("\t\t%s, %s\n", authorNode->name, key);
-
-                                    authorNode = authorNode->next;
-                                }
-
-
-                            } else {
-                                key = xmlNodeListGetString(xmlTree, entryNode->children, 1);
-                                printf(", %s\n", key);
-                            }
-                            
-                            xmlFree(key);
-                            entryNode = entryNode->next;
-                        }
-                    }
-                    
-                    node = node->next;
-                }
+                process_entry_node(xmlTree, node->children);
             }
         } else {
             printf("Error, parsing xml.\n");
@@ -72,6 +70,24 @@ int get_and_print_feed(char *host, char *port, char *path, int is_testing) {
     } else {
         printf("Error, converting string arr to xml arr\n");
     }
-    free(xmlResponse);
+
     return 0;
+}
+
+int get_and_print_feed(char *host, char *port, char *path, int is_testing) {
+    int result = connect_to_host(host, port, 0);
+    if (result) {
+        return result;
+    }
+    send_request(sock, host, port, path);
+
+    result = receive_data(is_testing);
+    if (result) {
+        return result;
+    }
+
+    result = process_xml();
+    
+    free(xmlResponse);
+    return result;
 }
