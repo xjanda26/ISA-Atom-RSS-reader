30df42a8e6be533d8033824df59e97af1405ba8b
Adam <xjanda26@stud.fit.vutbr.cz>
2022-11-11 16:07:16 +0100

    Atom documantation

--- src/atom.c
+++ src/atom.c
@@ -11,9 +11,12 @@ char *updatedAt;
 char *entryTitle;
 char *entryUri;
 
-/// @brief Function prints saved data about an article if their were provided in response.
+/// @brief Function prints saved data about an article 
+///        if their were provided in response.
 void print_entry() {
-    printf("%s\n", entryTitle);
+    if (entryTitle) {
+        printf("%s\n", entryTitle);
+    }
 
     if (optFlags[A_FLAG] > 0) {
         if (authorName) {
@@ -35,7 +38,7 @@ void print_entry() {
 
 }
 
-/// @brief Function free dynamicly allocated memory
+/// @brief Function frees dynamicly allocated memory.
 void free_entry_data() {
     free(entryTitle);
     if (optFlags[A_FLAG] > 0) {
@@ -57,7 +60,7 @@ void free_entry_data() {
     }
 }
 
-/// @brief Function processes and saves data about author of article
+/// @brief Function processes and saves data about author of article.
 /// @param doc Parsed XML object
 /// @param authorNode XML element within article
 void process_author_node(xmlDocPtr doc, xmlNodePtr authorNode) {
@@ -79,9 +82,9 @@ void process_author_node(xmlDocPtr doc, xmlNodePtr authorNode) {
     }
 }
 
-/// @brief Function processes and saves data about publishing date of arcticle
+/// @brief Function processes and saves data about publishing date of arcticle.
 /// @param doc Parsed XML object
-/// @param entryNode XML element of article
+/// @param entryNode Processed XML entry element (article node)
 void process_updated_node(xmlDocPtr doc, xmlNodePtr entryNode) {
     xmlChar *key = xmlNodeListGetString(doc, entryNode->children, 1);
     int keyLen = strlen((char *) key);
@@ -92,9 +95,9 @@ void process_updated_node(xmlDocPtr doc, xmlNodePtr entryNode) {
     xmlFree(key);
 }
 
-/// @brief Function processes and saves data about URI that refers to arcticle
+/// @brief Function processes and saves data about URI that refers to arcticle.
 /// @param doc Parsed XML object
-/// @param entryNode Processed XML element
+/// @param entryNode Processed XML entry element (article node)
 void process_link_node(xmlDocPtr doc, xmlNodePtr entryNode) {
     xmlBufferPtr nodeBuffer = xmlBufferCreate();
     xmlNodeDump(nodeBuffer, doc, entryNode,0,0);
@@ -125,7 +128,7 @@ void process_link_node(xmlDocPtr doc, xmlNodePtr entryNode) {
     xmlBufferFree(nodeBuffer);
 }
 
-/// @brief Function processes and save data about title of arcticle
+/// @brief Function processes and save data about title of arcticle.
 /// @param doc Parsed XML object
 /// @param entryNode Processed XML entry element (article node)
 void process_entry_title_node(xmlDocPtr doc, xmlNodePtr entryNode) {
@@ -138,7 +141,8 @@ void process_entry_title_node(xmlDocPtr doc, xmlNodePtr entryNode) {
     xmlFree(key);
 }
 
-/// @brief Function goes through every element of article. Pick only author, updated, link and title element.
+/// @brief Function goes through every element of article.
+///        Pick only author, updated, link and title element.
 /// @param doc Parsed XML object
 /// @param node XML element within feed element
 void process_entry_node(xmlDocPtr doc, xmlNodePtr node) {
@@ -188,7 +192,7 @@ void process_entry_node(xmlDocPtr doc, xmlNodePtr node) {
     }
 }
 
-/// @brief Function processes title of feed and prints it on STDOUT
+/// @brief Function processes title of feed and prints it on STDOUT.
 /// @param doc Parsed XML object
 /// @param node XML element within feed element
 void process_feed_title_node(xmlDocPtr doc, xmlNodePtr node) {
