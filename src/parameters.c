7f49a43fbf33d6370cf2620475c6aa895e215d24
Adam <xjanda26@stud.fit.vutbr.cz>
2022-11-12 11:43:10 +0100

    OPT fix - withou unit tests

--- src/parameters.c
+++ src/parameters.c
@@ -48,81 +48,19 @@ int parse_parameters (int argc, char *argv[]) {
     int opt, res, i = 2;
     int optErrFlag = 0, optPathMissFlag = 0;
     int optCertFileMissFlag = 0, optCertFolderMissFlag = 0;
-    int nextOpt = 1;
     
     if (argc < 2) {
         return error_msg(OPT_FEW);
     }
 
-    while (optind < argc) {
-        //printf("\nARGC: %i\n", argc);
-        //printf("OPTIND start %i\n==\n\n", optind);
+    while (i - 1 < argc) {
+        opt = getopt(argc, argv, "ac:C:f:Tu");
 
-        if (nextOpt) {
-            opt = getopt(argc, argv, "ac:C:f:Tu");
+        if (opt == 'c' || opt == 'C' || opt == 'f') {
+            optind --;
         }
 
-        //printf("OPT case: %c\n", opt);
-        //printf("OPTOPT case: %c\n", optopt);
-        //printf("OPTIND %i, i:%i\n", optind, i);
-
-        /*if (opt == -1) {
-            if (optopt == 'c' || optopt == 'C' || optopt == 'f') {
-                opt = optopt;
-                printf("New OPT case: %c\n", opt);
-            }
-        }*/
-
-        if(opt != -1 && (optind == i || (optind == i + 1 && (opt =='c' || opt == 'C' || opt == 'f')))) {
-            //printf("switch-case\n");
-            i++;
-            nextOpt = 1;
-            switch(opt) {
-                case 'a':
-                    optFlags[A_FLAG]++;
-                    break;
-                case 'c':
-                    optFlags[C_FLAG]++;
-                    certPath = optarg;
-                    if (certPath[0] == '-') {
-                        optCertFileMissFlag = 1;
-                    }
-                    i++;
-                    break;
-                case 'C':
-                    optFlags[CC_FLAG]++;
-                    certFolder = optarg;
-                    if (certFolder[0] == '-') {
-                        optCertFolderMissFlag = 1;
-                    }
-                    i++;
-                    break;
-                case 'f':
-                    optFlags[F_FLAG]++;
-                    filePath = optarg;
-                    if (filePath[0] == '-') {
-                        optPathMissFlag = 1;
-                    }
-                    i++;
-                    break;
-                case 'T':
-                    optFlags[T_FLAG]++;
-                    break;
-                case 'u':
-                    optFlags[U_FLAG]++;
-                    break;
-                case '?':
-                    if (optopt == 'f') {
-                        optPathMissFlag = 1;
-                    } else if (optopt == 'c') {
-                        optCertFileMissFlag = 1;
-                    } else if (optopt == 'C') {
-                        optCertFolderMissFlag = 1;
-                    } else {
-                        optErrFlag = 1;
-                    }
-            }
-        } else {
+        if (i != optind) {
             if (optFlags[DOMAIN_FLAG] > 0 ) {
                 if (optFlags[F_FLAG] > 0) {
                     return error_msg(OPT_MUL_DOMAINS);
@@ -139,20 +77,58 @@ int parse_parameters (int argc, char *argv[]) {
             optFlags[DOMAIN_FLAG]++;
 
             if (i == argc) {
-                //printf("optind ++\n");
-                optind++;
-            } else {
-                //printf("Something after written hostname\n");
-                if (optind == argc) {
-                    optind--;
-                }
-                nextOpt = 0;
-                i++;
+                break;
             }
-            
+
+            i++;
         }
-        
-        //printf("next itera ARGC:%i,OPTIND:%i\n", argc, optind);
+
+        switch(opt) {
+            case 'a':
+                optFlags[A_FLAG]++;
+                break;
+            case 'c':
+                optFlags[C_FLAG]++;
+                certPath = optarg;
+                if (certPath[0] == '-') {
+                    optCertFileMissFlag = 1;
+                }
+                i++;
+                break;
+            case 'C':
+                optFlags[CC_FLAG]++;
+                certFolder = optarg;
+                if (certFolder[0] == '-') {
+                    optCertFolderMissFlag = 1;
+                }
+                i++;
+                break;
+            case 'f':
+                optFlags[F_FLAG]++;
+                filePath = optarg;
+                if (filePath[0] == '-') {
+                    optPathMissFlag = 1;
+                }
+                i++;
+                break;
+            case 'T':
+                optFlags[T_FLAG]++;
+                break;
+            case 'u':
+                optFlags[U_FLAG]++;
+                break;
+            case '?':
+                if (optopt == 'f') {
+                    optPathMissFlag = 1;
+                } else if (optopt == 'c') {
+                    optCertFileMissFlag = 1;
+                } else if (optopt == 'C') {
+                    optCertFolderMissFlag = 1;
+                } else {
+                    optErrFlag = 1;
+                }
+        }
+        i++;
     }
 
     if (optErrFlag) {
